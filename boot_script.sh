9cecafbe1d6c9e6f69bf6d15f60aea8a03000b61
GanonKuppa <qtfdl94q@gmail.com>
2017-05-20 13:39:04 +0900

    modify

--- boot_script.sh
+++ boot_script.sh
@@ -4,5 +4,5 @@ source /home/pi/.nvm/nvm.sh
 
 forever start -c python3  /home/pi/BTtoMQTT_python/BTtoMQTT_GAMEPADtoBT.py
 forever start /home/pi/umouse_webapp/app.js
-forever start /home/pi/gamepad2MQTT-python/gamepad2MQTT.py
+forever start -c python /home/pi/gamepad2MQTT-python/gamepad2MQTT.py
 
