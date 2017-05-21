030ad56768a1a372163b8534cfabb7196d0dc949
GanonKuppa <qtfdl94q@gmail.com>
2017-05-21 11:18:15 +0900

    modify boot_script.sh

--- boot_script.sh
+++ boot_script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source /home/pi/.nvm/nvm.sh
+export NODE_PATH=/home/pi/.nvm/versions/node/v6.10.0/lib/node_modules
 
 forever start -c python3  /home/pi/BTtoMQTT_python/BTtoMQTT_GAMEPADtoBT.py
 forever start /home/pi/umouse_webapp/app.js
